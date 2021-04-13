c8f35cc412677a2c9c9712dc6f0ce5f61a74e88d
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-04-13 16:14:07 -0400

    feat: add tailwindcss and alpinejs
    Only use tailwindcss for now

---
+++ snowpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  mount: {
+    '_site': { url: '/', static: true, resolve: false },
+    'src/scripts': { url: '/scripts' },
+    'src/styles': { url: '/styles' },
+  },
+  plugins: [
+    '@snowpack/plugin-postcss',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eleventy',
+        watch: '$1 --watch',
+      },
+    ],
+  ],
+  packageOptions: {
+    NODE_ENV: true,
+    // source: 'remote',
+  },
+  buildOptions: {
+    clean: true,
+    out: 'dist',
+  },
+  devOptions: {
+    open: 'none',
+  },
+  // optimize: {
+  //   bundle: true,
+  //   minify: true,
+  //   target: 'es2020',
+  // },
+};
