3c082f881e999f24be4f22480c4bc755bd45ca55
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-04-13 14:05:14 -0400

    feat: add date filter and groupby for liquid template
    Why: to sort talks by ascending time

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
 module.exports = function (eleventyConfig) {
+  // Sort with `Array.sort`
+  eleventyConfig.addFilter("date_ascending", function(data) {
+    return data.sort(function(a, b) {
+      return new Date(a.date) - new Date(b.date); // sort by date - ascending
+    });
+  });
+
+  // Filter by date
+  eleventyConfig.addFilter("date_filter", function(data, date) {
+    return data.filter(function(item) {
+      return item.date === date; // sort by date - ascending
+    });
+  });
+
   return {
     dir: {
       input: 'src',
